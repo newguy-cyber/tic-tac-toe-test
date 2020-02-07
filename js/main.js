eafb088d052590c566b67c3455a42b36a70648bc
newguy21 <josegonzalez78721@yahoo.com>
2020-02-06 18:36:39 -0600

    finally added game logic

--- js/main.js
+++ js/main.js
@@ -4,49 +4,64 @@
 let playerOne = 1;
 let board;
 let winner;
-
 // Variables might include (board/turn/winner)
 
 /*------Cached Element References------*/
-
 // You might choose to put your game status here
 
 /*------Event Listeners------*/
-document.querySelector('.board').addEventListener('click', mouse)
+document.querySelector('.board').addEventListener('click', clickingbtn)
 // This is where you should put the event listener
 // for a mouse-click
 
 /*------Functions------*/
-function mouse(evt) {
-    let square = document.getElementById(evt.target.id)
-console.log(square);
+function clickingbtn(evt) {
+let square = document.getElementById(evt.target.id)
 let id = (evt.target.id);
 id = parseInt(id[2]);
-board[id] = 4;
-console.log(board);
+//The (parseInt) function turns a string into a interger
 //if..else statement blow
 if (playerOne === 1) {
-    square.innerHTML = 'A';
+    square.innerHTML = 'X';
+    board[id] = 1;
+    isWinner();
     playerOne = -1;
-
 } else{
-    square.innerHTML = 'B';
+    square.innerHTML = 'O';
+    board[id] = -1;
+    isWinner();
     playerOne = 1;
 } 
-}
 
+}
 function init(){
     board = [null, null, null, null, null, null, null, null, null];
     turn = 1;
     winner = null;
-
 }
 init();
+
+function isWinner () {
+    if (Math.abs(board[0]+board[1]+board[2] === 3) ||
+        Math.abs(board[3]+board[4]+board[5] === 3) ||
+        Math.abs(board[6]+board[7]+board[8] === 3) ||
+        Math.abs(board[0]+board[3]+board[6] === 3) ||
+        Math.abs(board[1]+board[4]+board[5] === 3) ||
+        Math.abs(board[2]+board[5]+board[8] === 3) ||
+        Math.abs(board[0]+board[4]+board[8] === 3) ||
+        Math.abs(board[2]+board[4]+board[6] === 3)){
+            document.getElementById('message').innerHTML = 'you won!';
+            console.log('iswinnerrunning')
+        
+        }
+}
+
 // Some functions you might choose to use:
 
 // Initialization function:
 // Where you set your initial state, setting up 
 // what the board will look like upon loading
+
 
 // On-Click function:
 // Set up what happens when one of the elements
@@ -63,6 +78,3 @@ init();
 // Displays the current state of the board
 // on the page, updating the elements to reflect
 // either X or O depending on whose turn it is
-var confettiSettings = { target: 'my-canvas' };
-var confetti = new ConfettiGenerator(confettiSettings);
-confetti.render();
